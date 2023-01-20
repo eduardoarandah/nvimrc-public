3214980aac65015d308339c420754e031b964b5b
Eduardo <eduardoarandah@gmail.com>
2023-01-20 11:19:18 -0600

    codeium

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -210,6 +210,17 @@ return require("packer").startup(function(use)
 		run = "npm install",
 	})
 
+	use({
+		"Exafunction/codeium.vim",
+		config = function()
+			vim.g.codeium_enabled = false
+			-- Change '<C-g>' here to any keycode you like.
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+		end,
+	})
+
 	--------------------------------------------------------------------------
 	-- LSP installer
 	-- Neovim plugin that allows you to seamlessly install LSP servers locally
